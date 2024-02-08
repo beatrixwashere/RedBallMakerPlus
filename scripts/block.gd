d55845fdbf013f506c48e080e624782a0f923569
thequeenbea <turtlezdude@gmail.com>
2024-02-08 00:37:53 -0600

    a whole lot of changes i'm too lazy to document
    mostly property editing

--- scripts/block.gd
+++ scripts/block.gd
@@ -6,7 +6,7 @@ class_name Block
 # data
 var type:String = "b"
 var name:String = "0"
-var behavior:String = "n"
+var flags:String = ""
 var mass:float = 0
 var friction:float = 1
 var restitution:float = 0.2
@@ -15,10 +15,12 @@ var polygon:PackedVector2Array = PackedVector2Array()
 var fill:Color = Color(0,0,0,1)
 # TODO: add textures
 var outline:Color = Color(0,0,0,1)
+var levelobj:Node
+var listobj:Node
 # encode into level format
 func encode_level()->String:
 	var output:String = ""
-	output += type+"_"+name+","+behavior+","+str(mass)+","+str(friction)+","+str(restitution)+":"+shape+","
+	output += type+"_"+name+","+flags+","+str(mass)+","+str(friction)+","+str(restitution)+":"+shape+","
 	for i in polygon: output += str(i.x)+","+str(i.y)+","
 	output = output.erase(output.length()-1)
 	output += ":0x"+fill.to_html(false)+",1:1,0x"+outline.to_html(false)+"|"
@@ -28,7 +30,7 @@ func save_data(file:FileAccess)->void:
 	# TODO: reduce size to the following comments
 	file.store_pascal_string(type) # store_8
 	file.store_pascal_string(name)
-	file.store_pascal_string(behavior) # store_8
+	file.store_pascal_string(flags) # store_8
 	file.store_float(mass)
 	file.store_float(friction)
 	file.store_float(restitution)
@@ -41,7 +43,7 @@ func load_data(file:FileAccess)->void:
 	# TODO: reduce size to the following comments
 	type = file.get_pascal_string() # store_8
 	name = file.get_pascal_string()
-	behavior = file.get_pascal_string() # store_8
+	flags = file.get_pascal_string() # store_8
 	mass = file.get_float()
 	friction = file.get_float()
 	restitution = file.get_float()
--- scripts/main.gd
+++ scripts/main.gd
@@ -4,12 +4,42 @@
 extends Node
 #region variables
 # level
-var level_blocks:Array[Block]
+var level_blocks:Dictionary
+var level_nidx:int = 0
+var level_selection:int = -1
 # ui input
 var is_moving_scene:bool = false
 #endregion
 # handles input
 func _input(event:InputEvent)->void:
+	# keyboard
+	if event is InputEventKey && level_selection == -1:
+		match event.keycode:
+			KEY_W:
+				%ui.position.y -= 15/$ui.zoom.x
+			KEY_A:
+				%ui.position.x -= 15/$ui.zoom.x
+			KEY_S:
+				%ui.position.y += 15/$ui.zoom.x
+			KEY_D:
+				%ui.position.x += 15/$ui.zoom.x
+			KEY_Q:
+				if %ui.zoom.x < 4.99:
+					%ui.zoom += Vector2(0.1,0.1)
+					%ui.scale = Vector2(1,1)/%ui.zoom
+					%ui/rbref.scale = %ui.zoom
+			KEY_E:
+				if %ui.zoom.x > 0.11:
+					%ui.zoom -= Vector2(0.1,0.1)
+					%ui.scale = Vector2(1,1)/%ui.zoom
+					%ui/rbref.scale = %ui.zoom
+			KEY_Z:
+				%ui.position = Vector2(0,0)
+				%ui.zoom = Vector2(2,2)
+				%ui.scale = Vector2(0.5,0.5)
+				%ui/rbref.scale = Vector2(2,2)
+			KEY_1:
+				if event.is_pressed(): new_block()
 	# mouse buttons
 	if event is InputEventMouseButton:
 		match event.button_index:
@@ -20,37 +50,103 @@ func _input(event:InputEvent)->void:
 					%ui.zoom += Vector2(0.1,0.1)
 					%ui.scale = Vector2(1,1)/%ui.zoom
 					%ui/rbref.scale = %ui.zoom
-					update_info()
 			MOUSE_BUTTON_WHEEL_DOWN:
 				if event.is_pressed() && %ui.zoom.x > 0.11:
 					%ui.zoom -= Vector2(0.1,0.1)
 					%ui.scale = Vector2(1,1)/%ui.zoom
 					%ui/rbref.scale = %ui.zoom
-					update_info()
 	# mouse motion
 	if event is InputEventMouseMotion:
 		if is_moving_scene:
-			%ui.position -= event.relative*%level.scale.x
-			update_info()
+			%ui.position -= event.relative/%ui.zoom.x
+	update_info()
 # creates a new block in the level
 func new_block()->void:
 	# create and setup block data
 	var n_data:Block = Block.new()
-	n_data.name = str(level_blocks.size())
-	n_data.polygon.append(Vector2(0,0)+(round(%ui.position)-Vector2(64,0)))
-	n_data.polygon.append(Vector2(50,0)+(round(%ui.position)-Vector2(64,0)))
-	n_data.polygon.append(Vector2(50,50)+(round(%ui.position)-Vector2(64,0)))
-	n_data.polygon.append(Vector2(0,50)+(round(%ui.position)-Vector2(64,0)))
-	level_blocks.append(n_data)
+	n_data.name = str(level_nidx)
+	n_data.polygon.append(Vector2(0,0)+round(%ui.position))
+	n_data.polygon.append(Vector2(50,0)+round(%ui.position))
+	n_data.polygon.append(Vector2(50,50)+round(%ui.position))
+	n_data.polygon.append(Vector2(0,50)+round(%ui.position))
 	# add block to scene
 	var n_block_obj:Polygon2D = Polygon2D.new()
 	n_block_obj.set_color(n_data.fill)
 	n_block_obj.set_polygon(n_data.polygon)
 	%level.add_child(n_block_obj)
+	var n_block_outline:Line2D = Line2D.new()
+	n_block_outline.points = n_data.polygon
+	n_block_outline.closed = true
+	n_block_outline.width = 2
+	n_block_outline.default_color = n_data.outline
+	n_block_obj.add_child(n_block_outline)
+	n_data.levelobj = n_block_obj
 	# add block to list
-	var n_block_list:Control = load("res://scenes/objs/list_block.tscn").instantiate() as Control
+	var n_block_list:Control = load("res://scenes/block/list_block.tscn").instantiate() as Control
 	n_block_list.get_node("text").text = n_data.name
-	%ui/menus/obj_list/container/list.add_child(n_block_list)
+	n_block_list.get_node("button").connect("button_down", edit_block.bind(level_nidx))
+	%blocklist.add_child(n_block_list)
+	%blocklist.move_child(n_block_list, \
+	%blocklist.get_child_count()-2)
+	n_data.listobj = n_block_list
+	level_blocks[level_nidx] = n_data
+	level_nidx += 1
+func edit_block(idx:int)->void:
+	close_edits()
+	# helper function for new list
+	var c_edit_obj:Callable = \
+	func c_edit_obj_func(i_name:String, i_type:String, i_var:Variant, i_popup:String = "")->void:
+		var n_edit_obj:Control = load("res://scenes/block/edit_block.tscn").instantiate() as Control
+		n_edit_obj.name = i_name
+		n_edit_obj.get_node("text").text = i_name
+		n_edit_obj.get_node("edit").placeholder_text = i_type
+		n_edit_obj.get_node("edit").text = str(i_var)
+		if i_popup != "":
+			n_edit_obj.get_node("edit").visible = false
+			n_edit_obj.get_node("popup_edit").visible = true
+			n_edit_obj.get_node("popup_edit").connect("button_down", \
+			n_edit_obj.get_node(i_popup).set_visible.bind(true))
+			match i_popup:
+				"popup_color":
+					n_edit_obj.get_node("popup_color/color").color = i_var
+					pass
+		%editlist.add_child(n_edit_obj)
+		%editlist.move_child(n_edit_obj, \
+		%editlist.get_child_count()-2)
+	# setup list
+	# TODO: add rest of variables, especially polygon
+	c_edit_obj.call("name","string",level_blocks[idx].name)
+	c_edit_obj.call("flags","string",level_blocks[idx].flags)
+	c_edit_obj.call("mass","float",level_blocks[idx].mass)
+	c_edit_obj.call("friction","float",level_blocks[idx].friction)
+	c_edit_obj.call("restitution","float",level_blocks[idx].restitution)
+	c_edit_obj.call("fill","",level_blocks[idx].fill,"popup_color")
+	c_edit_obj.call("outline","",level_blocks[idx].outline,"popup_color")
+	# scene obj indicators
+	level_blocks[idx].levelobj.modulate = Color(1,1,1,0.5)
+	level_selection = idx
+# applies block edits
+func apply_edits()->void:
+	# set block data
+	level_blocks[level_selection].name = %editlist.get_node("name/edit").text
+	level_blocks[level_selection].flags = %editlist.get_node("flags/edit").text
+	level_blocks[level_selection].mass = %editlist.get_node("mass/edit").text.to_float()
+	level_blocks[level_selection].friction = %editlist.get_node("friction/edit").text.to_float()
+	level_blocks[level_selection].restitution = %editlist.get_node("restitution/edit").text.to_float()
+	level_blocks[level_selection].fill = %editlist.get_node("fill/popup_color/color").color
+	level_blocks[level_selection].outline = %editlist.get_node("outline/popup_color/color").color
+	# apply data to scene
+	level_blocks[level_selection].levelobj.color = level_blocks[level_selection].fill
+	level_blocks[level_selection].levelobj.get_child(0).default_color = level_blocks[level_selection].outline
+	level_blocks[level_selection].listobj.get_node("text").text = level_blocks[level_selection].name
+	close_edits()
+# removes the edit list
+func close_edits()->void:
+	if level_selection != -1: level_blocks[level_selection].levelobj.modulate = Color(1,1,1,1)
+	for i in %editlist.get_children(): 
+		%editlist.remove_child(i)
+	%editlist.add_child(Control.new())
+	level_selection = -1
 # opens level export file dialog
 func select_file(option:String)->void:
 	match option:
@@ -68,7 +164,7 @@ func select_file(option:String)->void:
 func export_level(filepath:String)->void:
 	# generate level string
 	var export_string:String = ""
-	for i in level_blocks:
+	for i in level_blocks.values():
 		export_string += i.encode_level()
 	# save file
 	var levelfile:FileAccess = FileAccess.open(filepath, FileAccess.WRITE)
@@ -78,7 +174,7 @@ func export_save(filepath:String)->void:
 	# save file
 	var savefile:FileAccess = FileAccess.open(filepath, FileAccess.WRITE)
 	savefile.store_32(level_blocks.size())
-	for i in level_blocks:
+	for i in level_blocks.values():
 		i.save_data(savefile)
 	print("export_save "+filepath)
 # imports the level from an rbmp save
@@ -87,22 +183,35 @@ func import_save(filepath:String)->void:
 	level_blocks.clear()
 	for i in %level.get_children():
 		i.queue_free()
+	level_nidx = 0
 	# load file
 	var savefile:FileAccess = FileAccess.open(filepath, FileAccess.READ)
 	var level_size:int = savefile.get_32()
 	for i in level_size:
 		var n_data:Block = Block.new()
 		n_data.load_data(savefile)
-		level_blocks.append(n_data)
-	# generate scene
-	for i in level_blocks:
 		var n_block_obj:Polygon2D = Polygon2D.new()
-		n_block_obj.set_color(i.fill)
-		n_block_obj.set_polygon(i.polygon)
+		n_block_obj.set_color(n_data.fill)
+		n_block_obj.set_polygon(n_data.polygon)
 		%level.add_child(n_block_obj)
-		var n_block_list:Control = load("res://scenes/objs/list_block.tscn").instantiate() as Control
-		n_block_list.get_node("text").text = i.name
-		%ui/menus/obj_list/container/list.add_child(n_block_list)
+		n_data.levelobj = n_block_obj
+		var n_block_outline:Line2D = Line2D.new()
+		n_block_outline.points = n_data.polygon
+		n_block_outline.closed = true
+		n_block_outline.width = 2
+		n_block_outline.default_color = n_data.outline
+		n_block_obj.add_child(n_block_outline)
+		var n_block_list:Control = load("res://scenes/block/list_block.tscn").instantiate() as Control
+		n_block_list.get_node("text").text = n_data.name
+		%blocklist.add_child(n_block_list)
+		%blocklist.move_child(n_block_list, \
+		%blocklist.get_child_count()-2)
+		n_data.listobj = n_block_list
+		level_blocks[level_nidx] = n_data
+		level_nidx += 1
 # updates row_info text
 func update_info()->void:
 	$ui/menus/row_info/position.text = "( "+str(round(%ui.position.x))+" , "+str(round(%ui.position.y))+" )"
+# closes the application
+func exit_app()->void:
+	get_tree().quit()
