c1892243b12cbaebcbc5eddef20f727465bd4a55
thequeenbea <turtlezdude@gmail.com>
2024-02-22 11:16:29 -0600

    change undos to rbmp format

--- scripts/states.gd
+++ scripts/states.gd
@@ -1,7 +1,7 @@
 # ------------- states.gd --------------
 # stores tool states for undoing/redoing
 # --------------------------------------
-# TODO: implement redos
+# TODO: implement redos and optimize to single actions instead of level reloading
 extends Node
 # variables
 var undo:Array[String]
@@ -9,22 +9,22 @@ var redo:Array[String]
 var idx:int = 0
 # add new undo state
 func add_undo()->void:
-	var target:String = "user://tempstate_"+str(idx)+".txt"
-	get_tree().current_scene.export_level(target)
+	var target:String = "user://tempstate_"+str(idx)+".rbmp"
+	get_tree().current_scene.export_save(target)
 	undo.append(target)
 	idx += 1
 # undo an action
 func load_undo()->void:
 	if undo.size() > 0:
 		var target:String = undo.pop_back()
-		get_tree().current_scene.import_level(target)
-		DirAccess.remove_absolute(undo.pop_back())
+		get_tree().current_scene.import_save(target)
+		for i in 3: DirAccess.remove_absolute(undo.pop_back())
 		#redo.append(target)
 # redo an action
 func load_redo()->void:
 	if redo.size() > 0:
 		var target:String = redo.pop_back()
-		get_tree().current_scene.import_level(target)
+		get_tree().current_scene.import_save(target)
 		DirAccess.remove_absolute(undo.pop_back())
 		undo.append(target)
 # clear undo/redo list and files
